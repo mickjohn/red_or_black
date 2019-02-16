34c18b2a990519b2db463aaf70a49742df626f30
Michael Ashe <mickjohnashe@hotmail.com>
2019-02-16 22:05:01 +0000

    Updating deps

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,8 @@ fn main() {
         env::set_var("RUST_LOG", "websocket_red_or_black=debug");
     }
 
+    env_logger::init();
+
     // Read config from env vars
     let address =
         env::var("RED_OR_BLACK_WEBSERVER_ADDRESS").unwrap_or_else(|_| "127.0.0.1".to_string());
